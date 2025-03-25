28cbfe4ca7480c86b09cc837844cb1fb895600e3
Alexey_Viktorovich_Golik <golikalexey@list.ru>
2025-03-25 23:41:00 +0700

    25_03_2025

--- FirstProject/app/src/main/java/com/example/firstproject/MainActivity.java
+++ FirstProject/app/src/main/java/com/example/firstproject/MainActivity.java
@@ -1,12 +1,11 @@
 package com.example.firstproject;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -14,90 +13,55 @@ import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
-    //private TextView primary_text1;
-    //private TextView primary_text2;
-    //private Button test_button2;
-    private TextView result;
-    private EditText num1;
-    private EditText num2;
-    private Button plus;
-    private Button minus;
-    private Button umnozhenie;
-    private Button delenie;
+    private TextView primary_text2;
+    private Button button_open_music;
+    private Button button_open_calculator;
+    private Button test_button2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.caljkuljator_1);
-        //ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.calc1), (v, insets) -> {
-       //     Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-       //     v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-       //     return insets;
-       // });
+        setContentView(R.layout.main_homework);
 
-        result = findViewById(R.id.calc_result);
-        num1 = findViewById(R.id.number1);
-        num2 = findViewById(R.id.number2);
-        plus = findViewById(R.id.button_plus);
-        minus = findViewById(R.id.button_minus);
-        umnozhenie = findViewById(R.id.button_umn);
-        delenie = findViewById(R.id.button_delen);
+        primary_text2 = findViewById(R.id.text_hw2);
+        button_open_music = findViewById(R.id.button_open_music);
+        button_open_calculator = findViewById(R.id.button_open_calculator);
+        test_button2 = findViewById(R.id.button_text_hw);
 
-        plus.setOnClickListener(new View.OnClickListener() {
+        test_button2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Double number1 = Double.parseDouble(num1.getText().toString());
-                Double number2 = Double.parseDouble(num2.getText().toString());
-                result.setText(String.valueOf(number1 + number2));
+                primary_text2.setText("HELLO HELLO WORLD");
+                primary_text2.setTextSize(30);
             }
         });
 
-        minus.setOnClickListener(new View.OnClickListener() {
+        button_open_music.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Double number1 = Double.parseDouble(num1.getText().toString());
-                Double number2 = Double.parseDouble(num2.getText().toString());
-                result.setText(String.valueOf(number1 - number2));
+                Intent intent = new Intent(MainActivity.this, MusicActivity.class);
+                startActivity(intent);
             }
         });
 
-        umnozhenie.setOnClickListener(new View.OnClickListener() {
+        button_open_calculator.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Double number1 = Double.parseDouble(num1.getText().toString());
-                Double number2 = Double.parseDouble(num2.getText().toString());
-                result.setText(String.valueOf(number1 * number2));
+                Intent intent = new Intent(MainActivity.this, CalculatorActivity.class);
+                startActivity(intent);
             }
         });
 
-        delenie.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Double number1 = Double.parseDouble(num1.getText().toString());
-                Double number2 = Double.parseDouble(num2.getText().toString());
-                if(number2 == 0){
-                    result.setText("Input error");
-                }else{
-                    result.setText(String.valueOf(number1 / number2));
-                }
-            }
-        });
-
-        //primary_text2 = findViewById(R.id.text_hw2);
-        //test_button2 = findViewById(R.id.button_hw2);
-
-        //test_button2.setOnClickListener(new View.OnClickListener() {
-        //    @Override
-        //    public void onClick(View v) {
-        //        primary_text2.setText("HELLO HELLO WORLD");
-        //        primary_text2.setTextSize(30);
-        //    }
-        //});
     }
-    //public void sayHello(View view){
-    //    primary_text1 = findViewById(R.id.text_hw1);
-    //    primary_text1.setText("Hello world! Hello, Android!");
-    //    primary_text1.setTextSize(30);
+
+    //public void musicActivity(View view){
+    //    Intent intent = new Intent(this, MusicActivity.class);
+    //    startActivity(intent);
     //}
+
+    //public void calculatorActivity(View view){
+    //    Intent intent = new Intent(this, CalculatorActivity.class);
+    //    startActivity(intent);
+    //}
+
 }
---
+++ FirstProject/app/src/main/java/com/example/firstproject/MusicActivity.java
@@ -0,0 +1,35 @@
+package com.example.firstproject;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.view.View;
+import android.media.MediaPlayer;
+
+public class MusicActivity extends AppCompatActivity {
+    private Button back_music;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.music);
+
+        back_music = findViewById(R.id.back_music);
+
+        back_music.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MusicActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+}
