4c980eb35d7cdc687374f27bcd21d473f203eb19
Alexey_Viktorovich_Golik <golikalexey@list.ru>
2025-03-25 18:46:30 +0700

    Calculators

--- FirstProject/app/src/main/java/com/example/firstproject/MainActivity.java
+++ FirstProject/app/src/main/java/com/example/firstproject/MainActivity.java
@@ -30,11 +30,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.caljkuljator_1);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.calc1), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        //ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.calc1), (v, insets) -> {
+       //     Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+       //     v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+       //     return insets;
+       // });
 
         result = findViewById(R.id.calc_result);
         num1 = findViewById(R.id.number1);
