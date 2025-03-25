8d0e7fbd46b6c694d158aa959d9b398882d36526
Alexey_Viktorovich_Golik <golikalexey@list.ru>
2025-03-26 00:09:49 +0700

    SecondProject

---
+++ SecondProject/app/src/main/java/com/example/secondproject/CalculatorActivity.java
@@ -0,0 +1,87 @@
+package com.example.secondproject;
+
+import android.content.Intent;
+import android.os.Bundle;
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.view.View;
+
+public class CalculatorActivity extends AppCompatActivity {
+
+    private Button back_calc;
+    private TextView result;
+    private EditText num1;
+    private EditText num2;
+    private Button plus;
+    private Button minus;
+    private Button umnozhenie;
+    private Button delenie;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.calculator);
+
+        result = findViewById(R.id.calc_result);
+        num1 = findViewById(R.id.number1);
+        num2 = findViewById(R.id.number2);
+        plus = findViewById(R.id.button_plus);
+        minus = findViewById(R.id.button_minus);
+        umnozhenie = findViewById(R.id.button_umn);
+        delenie = findViewById(R.id.button_delen);
+        back_calc = findViewById(R.id.back_calc);
+
+        plus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Double number1 = Double.parseDouble(num1.getText().toString());
+                Double number2 = Double.parseDouble(num2.getText().toString());
+                result.setText(String.valueOf(number1 + number2));
+            }
+        });
+
+        minus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Double number1 = Double.parseDouble(num1.getText().toString());
+                Double number2 = Double.parseDouble(num2.getText().toString());
+                result.setText(String.valueOf(number1 - number2));
+            }
+        });
+
+        umnozhenie.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Double number1 = Double.parseDouble(num1.getText().toString());
+                Double number2 = Double.parseDouble(num2.getText().toString());
+                result.setText(String.valueOf(number1 * number2));
+            }
+        });
+
+        delenie.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Double number1 = Double.parseDouble(num1.getText().toString());
+                Double number2 = Double.parseDouble(num2.getText().toString());
+                if(number2 == 0){
+                    result.setText("Input error");
+                }else{
+                    result.setText(String.valueOf(number1 / number2));
+                }
+            }
+        });
+
+        back_calc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(CalculatorActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
--- SecondProject/app/src/main/java/com/example/secondproject/MainActivity.java
+++ SecondProject/app/src/main/java/com/example/secondproject/MainActivity.java
@@ -1,19 +1,56 @@
 package com.example.secondproject;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView primary_text2;
+    private Button button_open_music;
+    private Button button_open_calculator;
+    private Button test_button2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        primary_text2 = findViewById(R.id.text_hw2);
+        button_open_music = findViewById(R.id.button_open_music);
+        button_open_calculator = findViewById(R.id.button_open_calculator);
+        test_button2 = findViewById(R.id.button_text_hw);
+
+        test_button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                primary_text2.setText("HELLO HELLO WORLD");
+                primary_text2.setTextSize(30);
+            }
+        });
+
+        button_open_music.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MusicActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        button_open_calculator.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, CalculatorActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
