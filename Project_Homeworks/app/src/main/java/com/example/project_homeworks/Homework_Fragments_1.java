da9585eef4f0af0766bd1cf6fe5b9fbfacfaaf1a
Alexey_Viktorovich_Golik <golikalexey@list.ru>
2025-05-19 18:23:02 +0700

    Fragments_1

---
+++ Project_Homeworks/app/src/main/java/com/example/project_homeworks/Homework_Fragments_1.java
@@ -0,0 +1,19 @@
+package com.example.project_homeworks;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Homework_Fragments_1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homework_fragments1);
+
+    }
+}
